33785a6b2e6e0edfc57be0bb8e35f1e5dc17893b
zhongtai <zr2208@columbia.edu>
2019-05-03 12:29:26 -0400

    simple

--- 4840project_mouse/lab2/lab2.c
+++ 4840project_mouse/lab2/lab2.c
@@ -4,42 +4,18 @@
  *
  * Zhongtai Ren, zr2208
  */
-#include "fbputchar.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "usbmouse.h"
-#include <pthread.h>
 
-/* Update SERVER_HOST to be the IP address of
- * the chat server you are connecting to
- */
-/* micro36.ee.columbia.edu */
-// #define SERVER_HOST "128.59.148.182"
-// #define SERVER_HOST "160.39.249.225"
-// #define SERVER_HOST "160.39.248.109"
-// #define SERVER_PORT 42000
 
 #define BUFFER_SIZE 128
 
-/*
- * References:
- *
- * http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
- * http://www.thegeekstuff.com/2011/12/c-socket-programming/
- * 
- */
-
-// int sockfd; /* Socket file descriptor */
-
 struct libusb_device_handle *mouse;
 uint8_t endpoint_address;
-
-// pthread_t network_thread;
-// void *network_thread_f(void *);
 
 int main()
 {
@@ -56,89 +32,30 @@ int main()
     exit(1);
   }
 
-  /* Draw rows of asterisks across the top and bottom of the screen */
-  for (col = 0 ; col < 64 ; col++) {
-    fbputchar('*', 0, col);
-    fbputchar('*', 23, col);
-  }
-
-  fbputs("Hello CSEE 4840 World!", 4, 10);
-
   /* Open the mouse */
   if ( (mouse = openmouse(&endpoint_address)) == NULL ) {
     fprintf(stderr, "Did not find a mouse\n");
     exit(1);
   }
     
-  /* Create a TCP communications socket */
-  // if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-  //   fprintf(stderr, "Error: Could not create socket\n");
-  //   exit(1);
-  // }
 
-  // /* Get the server address */
-  // memset(&serv_addr, 0, sizeof(serv_addr));
-  // serv_addr.sin_family = AF_INET;
-  // serv_addr.sin_port = htons(SERVER_PORT);
-  // if ( inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
-  //   fprintf(stderr, "Error: Could not convert host IP \"%s\"\n", SERVER_HOST);
-  //   exit(1);
-  // }
-
-  // /* Connect the socket to the server */
-  // if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-  //   fprintf(stderr, "Error: connect() failed.  Is the server running?\n");
-  //   exit(1);
-  // }
-
-  /* Start the network thread */
-  // pthread_create(&network_thread, NULL, network_thread_f, NULL);
-
-  /* Look for and handle keypresses */
   for (;;) 
   {
     flg1 = libusb_interrupt_transfer(mouse, endpoint_address,
 			      (unsigned char *) &packet, sizeof(packet),
 			      &transferred, 0);
-    // if (flg1 == 0) {
-    //   sprintf("libusb_interrupt_transfer is: ","%d\n", flg1);
-    //   sprintf("transferred is: ", "%d\n", transferred);
-    //   sprintf("sizeof packet is: ", "%d\n",sizeof(packet));
-    // }
-    printf("%d\n", flg1);
+    // printf("%d\n", flg1);
     
 
     if (transferred == sizeof(packet)) {
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.pos_x,
 	      packet.pos_y);
       printf("%s\n", keystate);
-      fbputs(keystate, 6, 0);
       if (packet.pos_y == 0x29) { /* ESC pressed? */
 	       break;
       }
     }
   }
 
-  /* Terminate the network thread */
-  // pthread_cancel(network_thread);
-
-  /* Wait for the network thread to finish */
-  // pthread_join(network_thread, NULL);
-
   return 0;
 }
-
-// void *network_thread_f(void *ignored)
-// {
-//   char recvBuf[BUFFER_SIZE];
-//   int n;
-//   /* Receive data */
-//   while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
-//     recvBuf[n] = '\0';
-//     printf("%s", recvBuf);
-//     fbputs(recvBuf, 8, 0);
-//   }
-
-//   return NULL;
-//}
-
