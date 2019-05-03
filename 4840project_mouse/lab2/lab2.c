19f300d04c9e07a3e16f7b285c8e689a52894085
zhongtai <zr2208@columbia.edu>
2019-05-03 13:22:23 -0400

    debug

--- 4840project_mouse/lab2/lab2.c
+++ 4840project_mouse/lab2/lab2.c
@@ -47,17 +47,21 @@ int main()
         numx = -(0xFF - packet.pos_x + 1);
       }
       else { numx = packet.pos_x;}
-      if (px > 0 && px < 640) {
-        px = px + numx;
-      }
 
       if (packet.pos_y > 0x88) {
         numy = -(0xFF - packet.pos_y + 1);
       }
       else { numy = packet.pos_y;}
-      if (py > 0 && py < 480) {
-        py = py + numy;
-      }
+
+      if (px < 1) { px = 1}
+      else if (px > 0 && px < 640) { px = px + numx; }
+      else if (px > 639) { px = 639}
+      else {px = 320}
+
+      if (py < 1) { py = 1}
+      else if (py > 0 && py < 480) { py = py + numy; }
+      else if (py > 479) { px = 479}
+      else {py = 240}
 
       printf("position of x, y are: %d %d\n",px,py);
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.pos_x,
